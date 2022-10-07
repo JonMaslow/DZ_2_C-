2df0ea3731a9b0a5f346c0463dbe0e3bcc94135e
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-08 01:09:56 +0500

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void secondDigit(int result)
+{
+    if (result > 99 && result < 1000)
+    {
+        int secondDigit = result % 100 / 10;
+        Console.Write($"В трехзначном числе {result} вторая цифра {secondDigit}");
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не трехзначное число, попробуйте ещё раз");
+        return;
+    }
+}
+Console.WriteLine("Введите трёхзначное число: ");
+secondDigit(int.Parse(Console.ReadLine() ?? ""));
